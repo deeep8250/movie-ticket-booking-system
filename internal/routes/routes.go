7cd41ef99dbfa315fed86f196b2c4a080e7f3adb
deeep8250 <deepmondal825005@gmail.com>
2026-07-16 15:51:22 +0530

    added /ready in routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,10 +1,11 @@
 package routes
 
 import (
-	"github.com/deeep8250/movie-ticket-booking-system/internal/health"
+	"github.com/deeep8250/movie-ticket-booking-system/internal/handlers"
 	"github.com/gin-gonic/gin"
 )
 
 func Routes(r *gin.Engine) {
-	r.GET("/health", health.CheckHealth)
+	r.GET("/health", handlers.CheckHealth)
+	r.GET("/ready", handlers.CheckReady)
 }

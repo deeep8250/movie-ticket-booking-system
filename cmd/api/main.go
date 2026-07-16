4efdd04d2756851fed9fe95810b7bbb379b1cc2d
deeep8250 <deepmondal825005@gmail.com>
2026-07-16 15:36:32 +0530

    initilise db and redis

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/deeep8250/movie-ticket-booking-system/internal/config"
+	"github.com/deeep8250/movie-ticket-booking-system/internal/db"
 	"github.com/deeep8250/movie-ticket-booking-system/internal/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.DBinit()
+	db.RedisInit()
 	r := gin.Default()
 	routes.Routes(r)
 	r.Run(config.Load().Port)

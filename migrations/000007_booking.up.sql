730af660b88739220a859355479478227b083b65
deeep8250 <deepmondal825005@gmail.com>
2026-07-20 16:48:10 +0530

    added booking table

--- migrations/000007_booking.up.sql
+++ migrations/000007_booking.up.sql
@@ -2,8 +2,7 @@
         id bigserial primary key,
         user_id bigint references users(id) not null,
         show_id bigint references shows(id) not null,
-        status varchar(20) check(status in('confirmed','cancelled','pending','expired')),
+        status varchar(20) not null default 'pending' check(status in('confirmed','cancelled','pending','expired')),
         created_at timestamptz default now(),
-        updated_at timestamptz default now(),
-        unique(,show_id)
+        updated_at timestamptz default now()
     );

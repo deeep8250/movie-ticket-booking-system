32314c4890314d7ec455cd0d8bc767c033cf1eb3
deeep8250 <deepmondal825005@gmail.com>
2026-07-20 13:45:08 +0530

    fix mobile bug

--- migrations/000001_init_schema.up.sql
+++ migrations/000001_init_schema.up.sql
@@ -2,7 +2,7 @@ create table if not exists users(
  id bigserial primary key,
  username varchar not null,
  email varchar(200) not null unique,
- mobile int(20) not null unique,
+ mobile varchar(20) not null unique,
  created_at timestamptz not null default now(),
  updated_at timestamptz not null default now() 
 );

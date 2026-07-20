7bd706d13a029e89d9ab2ee939de9160b7a2af99
deeep8250 <deepmondal825005@gmail.com>
2026-07-20 14:15:20 +0530

    fixed some minor bugs in migration file

--- migrations/000003_halls_up.sql
+++ migrations/000003_halls_up.sql
@@ -2,7 +2,8 @@ create table if not exists halls(
     id bigserial primary key,
     theater_id bigint references theaters(id) not null,
     hall_name varchar not null,
-    total_seats int not null,
     created_at timestamptz not null default now(),
-    updated_at timestamptz not null default now()
+    updated_at timestamptz not null default now(),
+
+    unique(theater_id,hall_name)
 );
--- migrations/000004_movies.up.sql
+++ migrations/000004_movies.up.sql
@@ -1,7 +1,7 @@
 create table if not exists movies(
     id bigserial primary key,
     title varchar(200)  not null,
-    description text(200) not null,
+    description text not null,
     language varchar(200) not null,
     duration_min int not null check(duration_min>0),
     release_date date not null,

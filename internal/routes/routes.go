816e43d17ac3be903da2239470c8e5e9d780042f
deeep8250 <deepmondal825005@gmail.com>
2026-07-21 03:00:07 +0530

    added routes for theater

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,11 +1,21 @@
 package routes
 
 import (
-	"github.com/deeep8250/movie-ticket-booking-system/internal/handlers"
+	"github.com/deeep8250/movie-ticket-booking-system/internal/theaters"
+	statusandhealth "github.com/deeep8250/movie-ticket-booking-system/status_and_health"
 	"github.com/gin-gonic/gin"
 )
 
 func Routes(r *gin.Engine) {
-	r.GET("/health", handlers.CheckHealth)
-	r.GET("/ready", handlers.CheckReady)
+	r.GET("/health", statusandhealth.CheckHealth)
+	r.GET("/ready", statusandhealth.CheckReady)
+
+	theaterRepo := theaters.NewTheaterRepo()
+	theaterService := theaters.NewTheaterService(theaterRepo)
+	theaterHandler := theaters.NewTheaterHandler(theaterService)
+
+	p := r.Group("/public")
+	{
+		p.GET("/theaters", theaterHandler.GetTheaters)
+	}
 }

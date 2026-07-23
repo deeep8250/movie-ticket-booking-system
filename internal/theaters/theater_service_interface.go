327556688402185f335df9f94661b035eace8156
deeep8250 <deepmondal825005@gmail.com>
2026-07-23 07:48:34 +0530

    change the seatservice return type

--- internal/theaters/theater_service_interface.go
+++ internal/theaters/theater_service_interface.go
@@ -4,11 +4,10 @@ import (
 	"context"
 
 	"github.com/deeep8250/movie-ticket-booking-system/internal/dto"
-	"github.com/deeep8250/movie-ticket-booking-system/internal/models"
 )
 
 type TheaterServiceInterface interface {
 	GetTheatersService(c context.Context) ([]dto.Theater, error)
 	GetShowsService(c context.Context, TheaterId int) ([]dto.TheaterShows, error)
-	GetSeatsService(c context.Context, showsId int) (*models.SeatsInShows, error)
+	GetSeatsService(c context.Context, showsId int) (*dto.SeatsInShows, error)
 }

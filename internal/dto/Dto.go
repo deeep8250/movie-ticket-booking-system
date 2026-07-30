4c789433832a9e58f499c0e15824b2cda43a7ae0
deeep8250 <deepmondal825005@gmail.com>
2026-07-30 17:15:45 +0530

    add dto for BookingHistory

--- internal/dto/Dto.go
+++ internal/dto/Dto.go
@@ -58,11 +58,19 @@ type BookingsDetails struct {
 	CreatedAt   time.Time `json:"created_at"`
 }
 
-// create table if not exists seat_bookings(
-// id bigserial primary key,
-// booking_id bigint not null references bookings(id),
-// seat_id bigint not null references seats(id),
-// show_id bigint not null references shows(id),
-// created_at  timestamptz not null default now(),
-// unique(show_id,seat_id)
-// );
+type BookingHistory struct {
+	BookingID   int       `json:"booking_id"`
+	MovieName   string    `json:"movie_name"`
+	TheaterName string    `json:"theater_name"`
+	HallName    string    `json:"hall_name"`
+	SeatCount   int       `json:"seat_count"`
+	Status      string    `json:"status"`
+	TotalAmount float64   `json:"total_amount"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+type BookingHistoryWithUsrDtlsAtchd struct {
+	UserID          int              `json:"user_id"`
+	Username        string           `json:"username"`
+	BookingHitories []BookingHistory `json:"bookings"`
+}

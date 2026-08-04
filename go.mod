7819ac61831b787ada0776bc583b2fff381dc457
deeep8250 <deepmondal825005@gmail.com>
2026-08-04 21:01:55 +0530

    added jwt

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/goccy/go-yaml v1.19.2 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
--- internal/jwt/jwt.go
+++ internal/jwt/jwt.go
@@ -1,1 +1,23 @@
 package jwt
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func CreateJWT(userID int) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+
+}

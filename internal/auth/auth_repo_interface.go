b78baddf08c6c6557a45b8b8589f356f13754def
deeep8250 <deepmondal825005@gmail.com>
2026-08-04 21:34:56 +0530

    added service and repo auth interface

--- internal/auth/auth_repo_interface.go
+++ internal/auth/auth_repo_interface.go
@@ -1,1 +1,13 @@
 package auth
+
+import (
+	"context"
+
+	"github.com/deeep8250/movie-ticket-booking-system/internal/dto"
+	"github.com/deeep8250/movie-ticket-booking-system/internal/models"
+)
+
+type AuthRepo interface {
+	CreateUserRepo(c context.Context, useData dto.Users) error
+	GetUserRepo(c context.Context, userID int) (models.Users, error)
+}
--- internal/auth/auth_service_interface.go
+++ internal/auth/auth_service_interface.go
@@ -1,1 +1,12 @@
 package auth
+
+import (
+	"context"
+
+	"github.com/deeep8250/movie-ticket-booking-system/internal/dto"
+)
+
+type AuthService interface {
+	CreateUserService(c context.Context, useData dto.Users) error
+	GetUserService(c context.Context, userID int) (dto.Users, error)
+}

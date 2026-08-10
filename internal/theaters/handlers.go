0f3b97b691146a1311b589ce8b78b5eb3a11f797
deeep8250 <deepmondal825005@gmail.com>
2026-08-10 20:56:45 +0530

    implement seat unlock handler

--- internal/theaters/handlers.go
+++ internal/theaters/handlers.go
@@ -450,3 +450,125 @@ func (h *TheaterHandler) GetShowByMovieIdHandler(c *gin.Context) {
 	})
 
 }
+func (h *TheaterHandler) SeatLockHandler(c *gin.Context) {
+
+	userID, ok := c.Get("userID")
+	if !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "unauthorized user",
+		})
+		return
+	}
+
+	userIDInt, ok := userID.(int)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "invalid user id",
+		})
+		return
+	}
+	ShowId := c.Param("id")
+	ShowIdInt, err := strconv.Atoi(ShowId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input"})
+		return
+	}
+	type Uinput struct {
+		Seats []int `json:"seats" binding:"required,min=1,unique,dive,gt=0"`
+	}
+
+	var userInput Uinput
+	err = c.ShouldBindJSON(&userInput)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid input",
+		})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(c.Request.Context(), time.Second*5)
+	defer cancel()
+	err = h.service.SeatLockService(ctx, userIDInt, ShowIdInt, userInput.Seats)
+	if err != nil {
+		if strings.Contains(err.Error(), "seat already locked by you") {
+			c.JSON(http.StatusConflict, gin.H{
+				"error": "seat already locked by you",
+			})
+			return
+		}
+		if strings.Contains(err.Error(), "seat already locked by another user") {
+			c.JSON(http.StatusConflict, gin.H{
+				"error": "seat already locked by another user",
+			})
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "something went wrong"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": "selected seats are locked",
+	})
+
+}
+func (h *TheaterHandler) SeatUnLockHandler(c *gin.Context) {
+	userID, ok := c.Get("userID")
+	if !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "unauthorized user",
+		})
+		return
+	}
+
+	userIDInt, ok := userID.(int)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "invalid user id",
+		})
+		return
+	}
+
+	ShowId := c.Param("id")
+	ShowIdInt, err := strconv.Atoi(ShowId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input"})
+		return
+	}
+	type Uinput struct {
+		Seats []int `json:"seats" binding:"required,min=1,unique,dive,gt=0"`
+	}
+	var userInput Uinput
+	err = c.ShouldBindJSON(&userInput)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid input",
+		})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(c.Request.Context(), time.Second*5)
+	defer cancel()
+	err = h.service.SeatUnLockService(ctx, userIDInt, ShowIdInt, userInput.Seats)
+	if err != nil {
+		if strings.Contains(err.Error(), "seat is locked by another user") {
+			c.JSON(http.StatusConflict, gin.H{
+				"error": "seat is locked by another user",
+			})
+			return
+		}
+		if strings.Contains(err.Error(), "seat is already unlocked") {
+			c.JSON(http.StatusOK, gin.H{
+				"error": "seats already unlocked",
+			})
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "something went wrong"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": "selected seats are unlocked",
+	})
+}
